fc14c6d9e801cc9a9f99d1a2ec9974a901cd603f
m1rageLA <miragela.code@gmail.com>
2024-06-10 12:15:50 +0200

    Move add (project) funct-y to component

--- src/components/workspace/Projects.vue
+++ src/components/workspace/Projects.vue
@@ -3,64 +3,7 @@
         <div class="workspace__container">
             <div class="workspace__top-block">
                 <h2>Projects </h2>
-                <div class="py-5">
-                    <v-dialog v-model="dialog" max-width="500">
-                        <template v-slot:activator="{ props: activatorProps }">
-                            <v-btn class="text-none font-weight-regular" prepend-icon="mdi mdi-plus" text="Add project"
-                                variant="outlined" v-bind="activatorProps"></v-btn>
-                        </template>
-                        <v-card prepend-icon="mdi-account" title="Add project item">
-                            <v-card-text>
-                                <v-form @submit.prevent="submitForm">
-                                    <v-row dense>
-                                        <v-col cols="12" md="12" sm="6">
-                                            <v-text-field v-model="form.name" label="Title" hint="Web site"
-                                                required></v-text-field>
-                                        </v-col>
-                                        <v-col cols="12" md="12" sm="6">
-                                            <v-text-field v-model="form.description" label="Description"
-                                                hint="Description" required></v-text-field>
-                                        </v-col>
-                                        <v-autocomplete v-model="form.client" :disabled="isUpdating" :items="clients"
-                                            color="blue-grey-lighten-2" item-title="name" item-value="name"
-                                            label="Client" chips closable-chips>
-                                            <template v-slot:chip="{ props, item }">
-                                                <v-chip v-bind="props"
-                                                    :prepend-avatar="'http://localhost:8000/storage/' + item.raw.avatar"
-                                                    :text="item.raw.name"></v-chip>
-                                            </template>
-                                            <template v-slot:item="{ props, item }">
-                                                
-                                                <v-list-item v-bind="props"
-                                                    :prepend-avatar="'http://localhost:8000/storage/' + item.raw.avatar"
-                                                    :subtitle="item.raw.country" :title="item.raw.name"></v-list-item>
-                                                    
-                                            </template>
-                                            <template v-slot:prepend-item>
-                                                <v-btn class="my-2 ml-4 text-none font-weight-regular"
-                                                    prepend-icon="mdi mdi-plus" text="Quick add"
-                                                    @click="dialog2 = true" variant="outlined"></v-btn>
-
-                                            </template>
-                                        </v-autocomplete>
-                                        <v-col cols="12">
-                                            <v-file-input v-model="form.preview" :rules="rules"
-                                                accept="image/png, image/jpeg, image/bmp" label="Preview"
-                                                placeholder="Pick an avatar" prepend-icon="mdi-camera"></v-file-input>
-                                        </v-col>
-                                    </v-row>
-                                    <v-card-actions>
-                                        <v-spacer></v-spacer>
-                                        <v-btn text="Close" variant="plain" @click="dialog = false"></v-btn>
-                                        <v-btn color="primary" text="Save" variant="tonal" type="submit">Save</v-btn>
-                                    </v-card-actions>
-                                </v-form>
-                            </v-card-text>
-                            <AddClientDialog v-if="dialog2" :IsOpen="dialog2" :sendData="handleDataFromChild" />
-                            <v-divider></v-divider>
-                        </v-card>
-                    </v-dialog>
-                </div>
+                <AddProjectDialog :Dialog="dialog" :Dialog2="dialog2" :UpdateProjects="updateProjects"/>
             </div>
             <div class="list-markup">
                 <p></p>
@@ -114,15 +57,17 @@
 import { defineComponent, ref, onMounted } from 'vue';
 import LiElementProject from '../LiElementProject.vue';
 import axios from 'axios';
-import { toast } from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
 import AddClientDialog from './modals/AddClientDialog.vue';
+import AddProjectDialog from './modals/AddProjectDialog.vue';
+
 
 export default defineComponent({
     name: 'Clients',
     components: {
         LiElementProject,
-        AddClientDialog
+        AddClientDialog,
+        AddProjectDialog
     },
     data() {
         return {
@@ -256,46 +201,11 @@ export default defineComponent({
             }
         };
         const updateClients = async () => {
-            console.log("UPDATE");
             try {
                 const response = await axios.get('http://localhost:8000/api/clients');
                 clients.value = response.data;
             } catch (error) {
                 console.error('Ошибка:', error);
-            }
-        };
-
-        const submitForm = async () => {
-            console.log("submit");
-            const formData = new FormData();
-            formData.append('name', form.value.name);
-            formData.append('client', form.value.client);
-            formData.append('description', form.value.estimate);
-            if (form.value.preview) {
-                formData.append('preview', form.value.preview);
-            }
-            console.log(form.value);
-
-            try {
-                const response = await axios.post('http://localhost:8000/api/projects', formData, {
-                    headers: {
-                        'Content-Type': 'multipart/form-data'
-                    }
-                });
-                if (response.status === 201) {
-                    dialog.value = false;
-                    toast("Client created successfully!", {
-                        "theme": "auto",
-                        "type": "success",
-                        "position": "top-center",
-                        "autoClose": 1800,
-                        "dangerouslyHTMLString": true
-                    });
-                    updateProjects();
-                    updateClients();
-                }
-            } catch (error) {
-                console.error('Ошибка при отправке формы:', error);
             }
         };
 
@@ -313,7 +223,6 @@ export default defineComponent({
             rules,
             updateProjects,
             updateClients,
-            submitForm
         };
     }
 });
---
+++ src/components/workspace/modals/AddProjectDialog.vue
@@ -0,0 +1,143 @@
+<template>
+    <div class="py-5">
+        <v-dialog v-model="dialog" max-width="500">
+            <template v-slot:activator="{ props: activatorProps }">
+                <v-btn class="text-none font-weight-regular" prepend-icon="mdi mdi-plus" text="Add project"
+                    variant="outlined" v-bind="activatorProps"></v-btn>
+            </template>
+            <v-card prepend-icon="mdi-account" title="Add project item">
+                <v-card-text>
+                    <v-form @submit.prevent="submitForm">
+                        <v-row dense>
+                            <v-col cols="12" md="12" sm="6">
+                                <v-text-field v-model="form.name" label="Title" hint="Web site" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12" md="12" sm="6">
+                                <v-text-field v-model="form.description" label="Description" hint="Description"
+                                    required></v-text-field>
+                            </v-col>
+                            <v-autocomplete v-model="form.client" :disabled="isUpdating" :items="clients"
+                                color="blue-grey-lighten-2" item-title="name" item-value="name" label="Client" chips
+                                closable-chips>
+                                <template v-slot:chip="{ props, item }">
+                                    <v-chip v-bind="props"
+                                        :prepend-avatar="'http://localhost:8000/storage/' + item.raw.avatar"
+                                        :text="item.raw.name"></v-chip>
+                                </template>
+                                <template v-slot:item="{ props, item }">
+                                    <v-list-item v-bind="props"
+                                        :prepend-avatar="'http://localhost:8000/storage/' + item.raw.avatar"
+                                        :subtitle="item.raw.country" :title="item.raw.name"></v-list-item>
+                                </template>
+                                <template v-slot:prepend-item>
+                                    <v-btn class="my-2 ml-4 text-none font-weight-regular" prepend-icon="mdi mdi-plus"
+                                        text="Quick add" @click="dialog2 = true" variant="outlined"></v-btn>
+                                </template>
+                            </v-autocomplete>
+                            <v-col cols="12">
+                                <v-file-input v-model="form.preview" :rules="rules"
+                                    accept="image/png, image/jpeg, image/bmp" label="Preview"
+                                    placeholder="Pick an avatar" prepend-icon="mdi-camera"></v-file-input>
+                            </v-col>
+                        </v-row>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn text="Close" variant="plain" @click="dialog = false"></v-btn>
+                            <v-btn color="primary" text="Save" variant="tonal" type="submit">Save</v-btn>
+                        </v-card-actions>
+                    </v-form>
+                </v-card-text>
+                <AddClientDialog v-if="dialog2" :IsOpen="dialog2" :sendData="handleDataFromChild" />
+                <v-divider></v-divider>
+            </v-card>
+        </v-dialog>
+    </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import AddClientDialog from '../modals/AddClientDialog.vue';
+import axios from 'axios';
+import { toast } from 'vue3-toastify';
+
+export default {
+    name: 'AddProjectDialog',
+    components: {
+        AddClientDialog,
+    },
+    props: {
+        Dialog: Boolean,
+        Dialog2: Boolean,
+        UpdateProjects: {
+            type: Function,
+            required: true
+        },
+    },
+    setup(props) {
+        const dialog = ref(props.Dialog);
+        const dialog2 = ref(props.Dialog2);
+        const clients = ref([]);
+        const form = ref({
+            name: '',
+            description: '',
+            client: null,
+            preview: null,
+        });
+        const isUpdating = ref(false);
+
+        const submitForm = async () => {
+            const formData = new FormData();
+            formData.append('name', form.value.name);
+            formData.append('client', form.value.client);
+            formData.append('description', form.value.estimate);
+            if (form.value.preview) {
+                formData.append('preview', form.value.preview);
+            }
+            try {
+                const response = await axios.post('http://localhost:8000/api/projects', formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                });
+                if (response.status === 201) {
+                    dialog.value = false;
+                    toast("Client created successfully!", {
+                        "theme": "auto",
+                        "type": "success",
+                        "position": "top-center",
+                        "autoClose": 1800,
+                        "dangerouslyHTMLString": true
+                    });
+                    props.UpdateProjects();
+                    updateClients();
+                }
+            } catch (error) {
+                console.error('Ошибка при отправке формы:', error);
+            }
+        };
+        const updateClients = async () => {
+            try {
+                const response = await axios.get('http://localhost:8000/api/clients');
+                clients.value = response.data;
+            } catch (error) {
+                console.error('Ошибка:', error);
+            }
+        };
+        onMounted(() => {
+            updateClients();
+        });
+
+        return {
+            dialog,
+            dialog2,
+            form,
+            isUpdating,
+            clients,
+            submitForm,
+            updateClients
+        }
+    },
+}
+</script>
+
+<style scoped></style>
